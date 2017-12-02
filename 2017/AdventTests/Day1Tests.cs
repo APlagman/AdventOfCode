2af173f181e5aaf99c4770d7b7d62bf942b9051a
Alex Plagman <alexplagman@gmail.com>
2017-12-02 08:59:11 -0800

    Improve Day 1 tests

--- 2017/AdventTests/Day1Tests.cs
+++ 2017/AdventTests/Day1Tests.cs
@@ -10,20 +10,20 @@ namespace AdventTests
         [TestMethod]
         public void TestAdjacentDigits()
         {
-            Assert.AreEqual(Day1.SumIdenticalDigits("1122", 1), 3);
-            Assert.AreEqual(Day1.SumIdenticalDigits("1111", 1), 4);
-            Assert.AreEqual(Day1.SumIdenticalDigits("1234", 1), 0);
-            Assert.AreEqual(Day1.SumIdenticalDigits("91212129", 1), 9);
+            Assert.AreEqual(3, Day1.SumIdenticalDigits("1122", 1));
+            Assert.AreEqual(4, Day1.SumIdenticalDigits("1111", 1));
+            Assert.AreEqual(0, Day1.SumIdenticalDigits("1234", 1));
+            Assert.AreEqual(9, Day1.SumIdenticalDigits("91212129", 1));
         }
 
         [TestMethod]
         public void TestHalfwayDigits()
         {
-            Assert.AreEqual(Day1.SumIdenticalDigits("1212", "1212".Length / 2), 6);
-            Assert.AreEqual(Day1.SumIdenticalDigits("1221", "1221".Length / 2), 0);
-            Assert.AreEqual(Day1.SumIdenticalDigits("123425", "123425".Length / 2), 4);
-            Assert.AreEqual(Day1.SumIdenticalDigits("123123", "123123".Length / 2), 12);
-            Assert.AreEqual(Day1.SumIdenticalDigits("12131415", "12131415".Length / 2), 4);
+            Assert.AreEqual(6, Day1.SumIdenticalDigits("1212", "1212".Length / 2));
+            Assert.AreEqual(0, Day1.SumIdenticalDigits("1221", "1221".Length / 2));
+            Assert.AreEqual(4, Day1.SumIdenticalDigits("123425", "123425".Length / 2));
+            Assert.AreEqual(12, Day1.SumIdenticalDigits("123123", "123123".Length / 2));
+            Assert.AreEqual(4, Day1.SumIdenticalDigits("12131415", "12131415".Length / 2));
         }
     }
 }
