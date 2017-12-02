36f04420dc2968ef657468fe234b742757c57328
Alex Plagman <alexplagman@gmail.com>
2017-12-02 08:59:33 -0800

    Add Day 2 + tests

--- 2017/AdventSolutions/Day2.cs
+++ 2017/AdventSolutions/Day2.cs
@@ -1,4 +1,10 @@
-﻿namespace AdventSolutions
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace AdventSolutions
 {
     public class Day2 : AdventDay
     {
@@ -6,6 +12,66 @@
 
         protected override void RunImpl()
         {
+            string inputFile;
+            do
+            {
+                Console.WriteLine("Provide input file:");
+                inputFile = Console.ReadLine();
+            } while (!File.Exists(inputFile));
+            IEnumerable<string> input = File.ReadLines(inputFile);
+
+            Console.WriteLine("Answer to Part 1:");
+            int checksum = 0;
+            int lineNum = 0;
+            foreach (string line in input)
+            {
+                int range = Range(line);
+                Console.WriteLine($"Line {lineNum}: {range}");
+                checksum += range;
+                lineNum++;
+            }
+            Console.WriteLine(checksum);
+
+            Console.WriteLine("Answer to Part 2:");
+            checksum = 0;
+            lineNum = 0;
+            foreach (string line in input)
+            {
+                int quotient = DivideMultiplesIn(line);
+                Console.WriteLine($"Line {lineNum}: {quotient}");
+                checksum += quotient;
+                lineNum++;
+            }
+            Console.Write(checksum);
+        }
+
+        public static int Range(string line)
+        {
+            var numbersAsText = Regex.Replace(line, @"\s+", " ").Split(' ');
+            var numbers = from str in numbersAsText
+                         where int.TryParse(str, out _)
+                         select int.Parse(str);
+            return numbers.Max() - numbers.Min();
+        }
+
+        public static int DivideMultiplesIn(string line)
+        {
+            var numbersAsText = Regex.Replace(line, @"\s+", " ").Split(' ');
+            var parsedNumbers = from str in numbersAsText
+                          where int.TryParse(str, out _)
+                          select int.Parse(str);
+            int[] numbers = parsedNumbers.Where(num => num > 0).Distinct().ToArray();
+            for (int first = 0; first < numbers.Length; ++first)
+            {
+                for (int second = first + 1; second < numbers.Length; ++second)
+                {
+                    if (numbers[first] % numbers[second] == 0)
+                        return numbers[first] / numbers[second];
+                    if (numbers[second] % numbers[first] == 0)
+                        return numbers[second] / numbers[first];
+                }
+            }
+            throw new ArgumentException("Line does not contain a pair of evenly-divisible numbers.");
         }
     }
 }
---
+++ 2017/AdventTests/Day2Tests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AdventSolutions;
+
+namespace AdventTests
+{
+    [TestClass]
+    public class Day2Tests
+    {
+        [TestMethod]
+        public void TestRange()
+        {
+            Assert.AreEqual(8, Day2.Range("5 1 9 5"));
+            Assert.AreEqual(4, Day2.Range("7 5 3  "));
+            Assert.AreEqual(6, Day2.Range("2 4 6 8"));
+        }
+
+        [TestMethod]
+        public void TestDivision()
+        {
+            Assert.AreEqual(4, Day2.DivideMultiplesIn("5 9 2 8"));
+            Assert.AreEqual(3, Day2.DivideMultiplesIn("9 4 7 3"));
+            Assert.AreEqual(2, Day2.DivideMultiplesIn("3 8 6 5"));
+        }
+    }
+}
