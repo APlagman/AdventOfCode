bd77b2b873058c46dc6f7e3b2f35f51e3e3240df
Alex Plagman <alexplagman@gmail.com>
2017-12-09 19:58:47 -0800

    Add Day 8 + Tests

---
+++ 2017/AdventTests/Day8Tests.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AdventSolutions;
+
+namespace AdventTests
+{
+    [TestClass]
+    public class Day8Tests
+    {
+        [TestMethod]
+        public void TestMaximumValueAfterRegisterInstructions()
+        {
+            Assert.AreEqual(1, Day8.MaximumValueAfterRegisterInstructions(new[]
+            {
+                "b inc 5 if a > 1",
+                "a inc 1 if b < 5",
+                "c dec -10 if a >= 1",
+                "c inc -20 if c == 10"
+            }));
+        }
+
+        [TestMethod]
+        public void TestMaximumValueDuringRegisterInstructions()
+        {
+            Assert.AreEqual(10, Day8.MaximumValueDuringRegisterInstructions(new[]
+            {
+                "b inc 5 if a > 1",
+                "a inc 1 if b < 5",
+                "c dec -10 if a >= 1",
+                "c inc -20 if c == 10"
+            }));
+        }
+    }
+}
