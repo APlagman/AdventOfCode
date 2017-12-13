91c0dec2e5c5db716245862edd9c65e4d2d56951
Alex Plagman <alexplagman@gmail.com>
2017-12-13 13:04:07 -0800

    Day 13 + Tests

---
+++ 2017/AdventSolutions/Day13.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace AdventSolutions
+{
+    public class Day13 : AdventDay
+    {
+        protected override int Number => 13;
+
+        protected override void RunImpl()
+        {
+            // http://adventofcode.com/2017/day/13
+            var input = File.ReadLines(FileUtils.PromptForExistingFilePath());
+            Firewall firewall = new Firewall();
+            foreach (string line in input)
+            {
+                string[] args = line.Split(':');
+                firewall.AddLayer(int.Parse(args[0]), int.Parse(args[1].TrimStart(' ')));
+            }
+            Console.WriteLine("Answer to Part 1:");
+            Console.WriteLine(firewall.Pass().Severity);
+            Console.WriteLine("Answer to Part 2:");
+            Console.WriteLine(firewall.MinimumDelay());
+        }
+    }
+}
---
+++ 2017/AdventTests/Day13Tests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AdventSolutions;
+
+namespace AdventTests
+{
+    [TestClass]
+    public class Day13Tests
+    {
+        [TestMethod]
+        public void TestSeverity()
+        {
+            Firewall firewall = new Firewall();
+            firewall.AddLayer(0, 3);
+            firewall.AddLayer(1, 2);
+            firewall.AddLayer(4, 4);
+            firewall.AddLayer(6, 4);
+            Assert.AreEqual(24, firewall.Pass().Severity);
+        }
+
+        [TestMethod]
+        public void TestMinimumDelay()
+        {
+            Firewall firewall = new Firewall();
+            firewall.AddLayer(0, 3);
+            firewall.AddLayer(1, 2);
+            firewall.AddLayer(4, 4);
+            firewall.AddLayer(6, 4);
+            Assert.AreEqual(10, firewall.MinimumDelay());
+        }
+    }
+}
