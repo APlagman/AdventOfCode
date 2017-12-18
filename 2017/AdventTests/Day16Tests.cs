8b395862322f49f40814c9ac25fd21f8d287058f
Alex Plagman <alexplagman@gmail.com>
2017-12-17 19:23:54 -0800

    Add Day 16 + Tests

---
+++ 2017/AdventTests/Day16Tests.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AdventSolutions;
+
+namespace AdventTests
+{
+    [TestClass]
+    public class Day16Tests
+    {
+        [TestMethod]
+        public void TestDance()
+        {
+            Assert.AreEqual("baedc", Day16.Dance('e', new string[] { "s1", "x3/4", "pe/b" }));
+        }
+    }
+}
